f94321e0e76584472c1206fd5cddde36ef18032f
vitreusx <jakub_bednarz@protonmail.com>
2024-08-08 11:31:31 +0200

    Added new impl of config parser

--- rsrch/utils/config.py
+++ rsrch/utils/config.py
@@ -1,16 +1,16 @@
-"""Config parsing library."""
-
-import json
+import argparse
+import io
 import math
 import os
 import re
 import types
 import typing
-from argparse import Namespace
-from collections import UserDict
-from dataclasses import dataclass, fields, is_dataclass
+from collections.abc import MutableMapping
+from contextlib import contextmanager
+from dataclasses import fields, is_dataclass
+from functools import wraps
 from pathlib import Path
-from typing import Any, Mapping, Sequence, Type, TypeVar, get_args, get_origin
+from typing import Any, Callable, ParamSpec, Type, TypeVar, get_args, get_origin
 
 import numpy as np
 import pyparsing as pp
@@ -19,18 +19,19 @@ from ruamel.yaml import YAML
 yaml = YAML(typ="safe", pure=True)
 
 
-class AttrDict(dict):
-    def __getattr__(self, name):
-        return self[name]
+_open = open
 
 
-def to_attr_dict(x: Any):
-    if isinstance(x, dict):
-        return AttrDict({k: to_attr_dict(v) for k, v in x.items()})
-    elif isinstance(x, list):
-        return [to_attr_dict(v) for v in x]
-    else:
-        return x
+def open(path: str | Path):
+    """Open a YAML file."""
+    with _open(path, "r") as f:
+        return yaml.load(f)
+
+
+def load(content: str):
+    """Parse a string into a YAML object."""
+    buf = io.StringIO(content)
+    return yaml.load(buf)
 
 
 locator = pp.Empty().setParseAction(lambda s, l, t: l)
@@ -40,94 +41,219 @@ def locatedExpr(expr):
     return pp.Group(locator("start") + expr("value") + locator("end"))
 
 
-class Resolver:
-    EXPR = locatedExpr(pp.nested_expr("${", "}"))
+_items_as_attrs = False
+"""Whether attribute access for `Node` objects should be treated as item access."""
 
-    VAR_RE = r"(?P<up>\.+)(?P<var>[\w\.]+)"
+
+@contextmanager
+def enable_items_as_attrs():
+    """Enable accessing mapping items via attr access."""
+    global _items_as_attrs
+    prev = _items_as_attrs
+    _items_as_attrs = True
+    try:
+        yield
+    finally:
+        _items_as_attrs = prev
+
+
+@contextmanager
+def disable_items_as_attrs():
+    """Disable accessing mapping items via attr access."""
+    global _items_as_attrs
+    prev = _items_as_attrs
+    _items_as_attrs = False
+    try:
+        yield
+    finally:
+        _items_as_attrs = prev
+
+
+class Template:
+    """A string template.
+
+    A template is a string with expressions, denoted by opening `${` and closing `}`. These expressions can be replaced by their values using `render` function.
+    """
+
+    EXPR = locatedExpr(pp.nested_expr("${", "}"))
     EVAL_RE = r"((?P<resolver>[\w]+):)?(?P<expr>.*)"
 
-    def resolve(self, value: Any):
-        while True:
-            self.stack = [value]
-            value, is_same = self._single_pass(value)
-            if is_same:
-                break
-        return value
+    def __init__(self, text: str):
+        self.text = text
 
-    def _single_pass(self, x: Any):
-        is_same = True
-        if isinstance(x, dict):
-            new_x = {}
-            for k, v in x.items():
-                self.stack.append(v)
-                v, is_v_same = self._single_pass(v)
-                self.stack.pop()
-                is_same &= is_v_same
-                new_x[k] = v
-            x = new_x
-        elif isinstance(x, list):
-            new_x = []
-            for i, v in enumerate(x):
-                self.stack.append(v)
-                v, is_v_same = self._single_pass(v)
-                self.stack.pop()
-                is_same &= is_v_same
-                new_x.append(v)
-            x = new_x
-        elif isinstance(x, str):
-            new_x = self._eval_text(x)
-            is_same &= x == new_x
-            x = new_x
-        return x, is_same
+    def render(self, locals={}):
+        """Render a template, given a locals mapping."""
 
-    def _eval_text(self, text: str):
         exprs = []
-        for m in self.EXPR.search_string(text).as_list():
+        for m in self.EXPR.search_string(self.text).as_list():
             beg, _, end = m[0]
             exprs.append((beg, end))
 
-        if len(exprs) == 1 and exprs[0] == (0, len(text)):
-            return self._eval_expr(text[2:-1])
-        else:
-            cur, res = 0, []
-            for beg, end in exprs:
-                res.append(text[cur:beg])
-                eval_r = self._eval_expr(text[beg + 2 : end - 1])
-                if not isinstance(eval_r, str):
-                    eval_r = str(eval_r)
-                res.append(eval_r)
-                cur = end
-            res.append(text[cur:])
-            return "".join(res)
+        if len(exprs) == 1:
+            return self._eval_expr(self.text[2:-1], locals)
 
-    def _eval_expr(self, expr: str):
-        m = re.match(self.VAR_RE, expr)
-        if m is not None:
-            backtrack, name = len(m["up"]), m["var"]
-            value = self.stack[-backtrack - 1]
-            for part in name.split("."):
-                value = value[part]
-            return value
+        cur, res = 0, []
+        for beg, end in exprs:
+            res.append(self.text[cur:beg])
+            eval_r = self._eval_expr(self.text[beg + 2 : end - 1], locals)
+            if not isinstance(eval_r, str):
+                eval_r = str(eval_r)
+            res.append(eval_r)
+            cur = end
+        res.append(self.text[cur:])
+        return "".join(res)
 
+    def _eval_expr(self, expr: str, locals={}):
         m = re.match(self.EVAL_RE, expr)
-        if m is not None:
-            resolver = m["resolver"] or "eval"
-            if resolver == "eval":
-                g = {"math": math, "np": np}
-                for scope in self.stack[:-1]:
-                    if isinstance(scope, dict):
-                        g.update(scope)
-                g = to_attr_dict(g)
-                return eval(m["expr"], g)
-            elif resolver == "env":
-                return os.environ[m["expr"]]
-            else:
-                raise ValueError(f"Unknown resolver '{resolver}'")
+        if m is None:
+            raise ValueError(f"String '{expr}' is not a valid expression.")
 
-        raise ValueError(f"Could not interpret expression '{expr}'.")
+        resolver = m["resolver"] or "eval"
+        if resolver == "eval":
+            locals_ = {"math": math, "np": np, **locals}
+            with enable_items_as_attrs():
+                return eval(m["expr"], None, locals_)
+        elif resolver == "env":
+            return os.environ[m["expr"]]
+        else:
+            raise ValueError(f"Unknown resolver '{resolver}'")
+
+    def __repr__(self):
+        return repr(self.text)
 
 
-def cast(x, t):
+class Node(MutableMapping):
+    """A YAML node.
+
+    A node behaves much like a list or a dict, except:
+    - One can access the subnodes via attribute access.
+    - All the templates are automatically rendered.
+    """
+
+    def __init__(self, value, parent=None):
+        if isinstance(value, Node):
+            value = value.unwrapped
+        self.unwrapped = value
+        self.parent = parent
+
+    @property
+    def scope(self):
+        stack, cur = [], self
+        while cur is not None:
+            if isinstance(cur.unwrapped, dict):
+                stack.append(cur.unwrapped)
+            cur = cur.parent
+        stack.reverse()
+        scope = {k: v for scope in stack for k, v in scope.items()}
+        for k, v in scope.items():
+            if isinstance(v, (dict, list)):
+                scope[k] = Node(v)
+        return scope
+
+    def __getattr__(self, name: str):
+        if _items_as_attrs and not name.startswith("__"):
+            return self[name]
+        else:
+            return super().__getattribute__(name)
+
+    def __setattr__(self, name: str, value):
+        if _items_as_attrs and not name.startswith("__"):
+            self[name] = value
+        else:
+            super().__setattr__(name, value)
+
+    def __delattr__(self, name: str):
+        if _items_as_attrs and not name.startswith("__"):
+            del self[name]
+        else:
+            super().__delattr__(name)
+
+    def __getitem__(self, key):
+        with disable_items_as_attrs():
+            if isinstance(self.unwrapped, dict):
+                if key not in self:
+                    self[key] = {}
+
+            child = self.unwrapped[key]
+
+            if isinstance(child, str):
+                child = Template(child).render(self.scope)
+
+            if isinstance(child, (list, dict)):
+                child = Node(child, parent=self)
+
+            return child
+
+    def __contains__(self, key):
+        return key in self.unwrapped
+
+    def __setitem__(self, key, value):
+        with disable_items_as_attrs():
+            if isinstance(value, Node):
+                value = value.unwrapped
+            self.unwrapped[key] = value
+
+    def __delitem__(self, key):
+        del self.unwrapped[key]
+
+    def __len__(self):
+        return len(self.unwrapped)
+
+    def __iter__(self):
+        if isinstance(self.unwrapped, dict):
+            return iter(self.unwrapped)
+        else:
+            return (self[i] for i in range(len(self)))
+
+    def __repr__(self):
+        return repr(self.unwrapped)
+
+
+def _render_all(value):
+    if isinstance(value, Node):
+        unwrapped = value.unwrapped
+        if isinstance(unwrapped, dict):
+            return {k: _render_all(v) for k, v in value.items()}
+        elif isinstance(unwrapped, list):
+            return [_render_all(elem) for elem in value]
+    else:
+        return value
+
+
+def render_all(cfg):
+    cfg = Node(cfg)
+    cfg = _render_all(cfg)
+    return cfg
+
+
+def _use_preset(node_b: Node, preset, node_p: Node):
+    if not isinstance(preset, dict):
+        return preset
+
+    for k, v in preset.items():
+        with disable_items_as_attrs():
+            k2 = Template(k).render({**node_b.scope, **node_p.scope})
+        with enable_items_as_attrs():
+            exec(f"node_b.{k2} = _use_preset(node_b.{k2}, v, node_p[k])")
+
+    return node_b
+
+
+def use_preset(base: dict, preset: dict):
+    node_b, node_p = Node(base), Node(preset)
+    _use_preset(node_b, preset, node_p)
+
+
+def compose(base: dict, *presets: dict):
+    base = {**base}
+    for preset in presets:
+        use_preset(base, preset)
+    base = render_all(base)
+    base = _hide_private(base)
+    return base
+
+
+def _cast(x: Any, t: Type):
     t_args = get_args(t)
     t = get_origin(t) or t
 
@@ -143,13 +269,10 @@ def cast(x, t):
 
         for name in x:
             field = field_map[name]
-
             field_t = field.type
             if isinstance(field_t, str):
-                # For some insane reason, sometimes field.type is a name
-                # of the class, like 'str' or 'bool'
                 field_t = eval(field_t)
-            args[field.name] = cast(x[field.name], field_t)
+            args[field.name] = _cast(x[field.name], field_t)
 
         return t(**args)
     elif t in (typing.Union, typing.Optional, types.UnionType):
@@ -159,21 +282,21 @@ def cast(x, t):
                 return x
         for ti in t_args:
             try:
-                return cast(x, ti)
+                return _cast(x, ti)
             except:
                 pass
         raise ValueError(f"None of the variant types {t_args} match value {x}")
     elif t in (typing.Tuple, tuple):
-        return tuple([cast(xi, ti) for xi, ti in zip(x, t_args)])
+        return tuple([_cast(xi, ti) for xi, ti in zip(x, t_args)])
     elif t in (typing.List, typing.Set, list, set):
         elem_t = t_args[0]
-        return t([cast(xi, elem_t) for xi in x])
+        return t([_cast(xi, elem_t) for xi in x])
     elif t in (typing.Dict, dict):
         if len(t_args) > 0:
             kt, vt = t_args
         else:
             kt, vt = Any, Any
-        return {cast(k, kt): cast(xi, vt) for k, xi in x.items()}
+        return {_cast(k, kt): _cast(xi, vt) for k, xi in x.items()}
     elif t in (typing.Literal,):
         # For Literals, check if the value is one of the allowed values.
         if x not in t_args:
@@ -191,112 +314,84 @@ def cast(x, t):
         return x if isinstance(t, type) and isinstance(x, t) else t(x)
 
 
-T = TypeVar("T")
-
-
-def hide_private(x: Any):
+def _hide_private(x: Any):
     if isinstance(x, dict):
         r = {}
         for k, v in x.items():
             if isinstance(k, str) and k.startswith("_"):
                 continue
-            r[k] = hide_private(v)
+            r[k] = _hide_private(v)
         return r
     elif isinstance(x, list):
-        return [hide_private(xi) for xi in x]
+        return [_hide_private(xi) for xi in x]
     else:
         return x
 
 
-def unravel(x: Any):
-    """Recursively replace all keys in the dict of form <k1>.<k2>...: v
-    with k1: {k2: ... }."""
-
-    if isinstance(x, dict):
-        r = {}
-        for k, v in x.items():
-            parts = k.split(".")
-            cur = r
-            for part in parts[:-1]:
-                if part not in cur:
-                    cur[part] = {}
-                cur = cur[part]
-            cur[parts[-1]] = unravel(v)
-    elif isinstance(x, list):
-        r = [unravel(xi) for xi in x]
-    else:
-        r = x
-    return r
+T = TypeVar("T")
 
 
-def ravel(x: Any):
-    r = {}
+def cast(cfg: dict, typ: Type[T]) -> T:
+    cfg = _hide_private(cfg)
+    return _cast(cfg, typ)
 
-    def walk(cur, dest=None):
-        if isinstance(cur, dict):
-            for k, v in cur.items():
-                walk(v, k if dest is None else f"{dest}.{k}")
-        elif isinstance(cur, list):
-            for i, v in enumerate(cur):
-                walk(v, str(i) if dest is None else f"{dest}.{i}")
+
+def cli(
+    config_yml: str | Path,
+    presets_yml: str | Path | None = None,
+):
+    p = argparse.ArgumentParser()
+    p.add_argument(
+        "-c",
+        "--config-file",
+        type=Path,
+        default=Path(config_yml),
+        help="Path to config.yml file with default config values.",
+    )
+    if presets_yml is not None:
+        p.add_argument(
+            "-P",
+            "--presets-file",
+            type=Path,
+            default=Path(presets_yml),
+            help="Path to presets.yml file with available presets.",
+        )
+        p.add_argument(
+            "-p",
+            "--presets",
+            type=str,
+            nargs="+",
+            help="List of presets to be used.",
+        )
+
+    args, unk = p.parse_known_args()
+
+    base = open(args.config_file)
+
+    presets = []
+    if presets_yml is not None:
+        all_presets = open(args.presets_file)
+        for name in args.presets:
+            preset = eval(f"Node(all_presets).{name}")
+            presets.append(preset)
+
+    idx = 0
+    while idx < len(unk):
+        val = unk[idx]
+        if val.startswith("--"):
+            key = val.removeprefix("--")
+            stop = idx + 1
+            while stop < len(unk) and not unk[stop].startswith("--"):
+                stop += 1
+            vals = val[idx + 1 : stop]
+            if len(vals) == 1:
+                vals = vals[0]
+            presets.append({key: vals})
+            idx = stop
         else:
-            r[dest] = cur
+            idx += 1
 
-    walk(x)
-    return r
-
-
-def parse(data: dict, cls: Type[T]) -> T:
-    """Given a config dict, parse it into a config object of a given type."""
-    data = unravel(data)
-    data = Resolver().resolve(data)
-    data = hide_private(data)
-    return cast(data, cls)
+    return compose(base, *presets)
 
 
-def merge_(dst: Any, src: Any):
-    src = unravel(src)
-
-    if not isinstance(src, dict) or not isinstance(dst, dict):
-        return src
-
-    for k, v in src.items():
-        cur = dst
-        k_parts = k.split(".")
-        for k_part in k_parts[:-1]:
-            if not isinstance(cur.get(k_part), dict):
-                cur[k_part] = {}
-            cur = cur[k_part]
-        cur[k_parts[-1]] = merge_(cur.get(k_parts[-1]), v)
-    return dst
-
-
-def merge(*dicts: dict):
-    r = {}
-    for d in dicts:
-        r = merge_(r, d)
-    return r
-
-
-def load(path: str | Path):
-    """Load data from a .json or .yml file."""
-
-    path = Path(path)
-    if path.suffix in (".yml", ".yaml"):
-        with open(path, "r") as f:
-            return yaml.load(f)
-    elif path.suffix in (".json",):
-        with open(path, "r") as f:
-            return json.load(f)
-
-
-def add_preset_(cfg: dict, presets: dict, name: str):
-    """Update config dict with a preset. If the preset contains `$extends` key,
-    the indicated presets are added as well."""
-
-    preset = presets.get(name, {})
-    if "$extends" in preset:
-        for ext in preset["$extends"]:
-            add_preset_(cfg, presets, ext)
-        del preset["$extends"]
-    merge_(cfg, preset)
+__all__ = ["compose", "cast", "cli"]
