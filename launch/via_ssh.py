96e3dadfd2a9802b6e33bbd5a97540b2f5559571
vitreusx <jakub_bednarz@protonmail.com>
2024-08-30 12:31:36 +0200

    Added a SSH availability check before ops

--- launch/via_ssh.py
+++ launch/via_ssh.py
@@ -79,12 +79,16 @@ def main():
 
     args = p.parse_args()
 
-    hosts = args.hosts
-
     # For some reason, trying to read args.commands after sync_hosts call
     # results in [], as though the call cleared stdin.
     if args.mode == "grid":
         all_cmds = [line.strip() for line in args.commands]
+
+    hosts = args.hosts
+    for host in hosts:
+        CMD = ["ssh", host]
+        logging.info(shlex.join(CMD))
+        subprocess.run(CMD, stdout=DEVNULL).check_returncode()
 
     def sync_hosts():
         repo = git.Repo(
@@ -112,27 +116,22 @@ def main():
         with open(sync_host_path, "w") as f:
             f.write(sync_host)
 
-        try:
-            for host in hosts:
-                CMD = ["ssh", host, "mktemp", "sync_host.XXXXXXXX", "-p", "/tmp"]
-                logging.info(shlex.join(CMD))
-                remote_sync = subprocess.check_output(CMD).decode().rstrip()
+        for host in hosts:
+            CMD = ["ssh", host, "mktemp", "sync_host.XXXXXXXX", "-p", "/tmp"]
+            logging.info(shlex.join(CMD))
+            remote_sync = subprocess.check_output(CMD).decode().rstrip()
 
-                CMD = ["scp", sync_host_path, f"{host}:{remote_sync}"]
-                logging.info(shlex.join(CMD))
-                subprocess.run(CMD, stdout=DEVNULL).check_returncode()
+            CMD = ["scp", sync_host_path, f"{host}:{remote_sync}"]
+            logging.info(shlex.join(CMD))
+            subprocess.run(CMD, stdout=DEVNULL).check_returncode()
 
-                CMD = ["ssh", host, "bash", "-i", remote_sync]
-                logging.info(shlex.join(CMD))
-                subprocess.run(CMD).check_returncode()
+            CMD = ["ssh", host, "bash", "-i", remote_sync]
+            logging.info(shlex.join(CMD))
+            subprocess.run(CMD).check_returncode()
 
-                CMD = ["ssh", host, "rm", remote_sync]
-                logging.info(shlex.join(CMD))
-                subprocess.run(CMD).check_returncode()
-
-        except Exception as e:
-            logging.error(f"Sync on {host} was unsuccessful.")
-            raise e
+            CMD = ["ssh", host, "rm", remote_sync]
+            logging.info(shlex.join(CMD))
+            subprocess.run(CMD).check_returncode()
 
     def fail_if_dirty():
         repo = git.Repo(
@@ -148,27 +147,19 @@ def main():
         pivots = [int((i * len(all_cmds)) / len(hosts)) for i in range(len(hosts) + 1)]
         all_cmds = [all_cmds[start:end] for start, end in zip(pivots, pivots[1:])]
 
-        remotes = {}
+        for host, cmds in zip(hosts, all_cmds):
+            CMD = ["ssh", host, "mktemp", "cmd.XXXXXXXX", "-p", "/tmp"]
+            logging.info(shlex.join(CMD))
+            remote_cmd = subprocess.check_output(CMD).decode().rstrip()
 
-        try:
-            for host, cmds in zip(hosts, all_cmds):
-                CMD = ["ssh", host, "mktemp", "cmd.XXXXXXXX", "-p", "/tmp"]
-                logging.info(shlex.join(CMD))
-                remotes[host] = subprocess.check_output(CMD).decode().rstrip()
+            host_cmd = tempfile.mktemp(prefix="cmd.")
+            with open(host_cmd, "w") as f:
+                f.writelines(cmd + "\n" for cmd in cmds)
 
-                host_cmd = tempfile.mktemp(prefix="cmd.")
-                with open(host_cmd, "w") as f:
-                    f.writelines(cmd + "\n" for cmd in cmds)
+            CMD = ["scp", host_cmd, f"{host}:{remote_cmd}"]
+            logging.info(shlex.join(CMD))
+            subprocess.run(CMD, stdout=DEVNULL).check_returncode()
 
-                CMD = ["scp", host_cmd, f"{host}:{remotes[host]}"]
-                logging.info(shlex.join(CMD))
-                subprocess.run(CMD, stdout=DEVNULL).check_returncode()
-
-        except Exception as e:
-            logging.error(f"Sync on {host} was unsuccessful.")
-            raise e
-
-        for host in hosts:
             CMD = [
                 "ssh",
                 host,
@@ -177,7 +168,7 @@ def main():
                 "-s",
                 args.session,
                 "-d",
-                f"parallel -j{args.tasks_per_host} -a {remotes[host]}",
+                f"parallel -j{args.tasks_per_host} -a {remote_cmd}",
             ]
             logging.info(shlex.join(CMD))
             subprocess.run(CMD).check_returncode()
