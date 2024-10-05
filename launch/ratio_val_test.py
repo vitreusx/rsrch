414119e225788a8a882e27a87025d7cc439d509f
vitreusx <jakub_bednarz@protonmail.com>
2024-10-05 15:45:40 +0200

    Minor preset changes

--- launch/ratio_val_test.py
+++ launch/ratio_val_test.py
@@ -6,7 +6,7 @@ from .common import *
 from .sanity_check import sanity_check
 
 
-def low_ratio_test(test_name, suffix=""):
+def ratio_val_test(test_name, suffix=""):
     all_tests = []
 
     common_args = ["-p", "exp.ratio_val_test"]
@@ -16,8 +16,8 @@ def low_ratio_test(test_name, suffix=""):
     }
 
     seeds = [0, 1, 2]
-    envs = ATARI_100k_3
-    freqs = [64, 32, 16, 8, 4, 2]
+    envs = A100k_MONO[:3]
+    freqs = [64, 32, 16, 8, 4, 2, 1]
 
     for seed, env, freq in product(seeds, envs, freqs):
         options = {
@@ -38,10 +38,7 @@ def main():
     p.add_argument("--name", default="ratio-val-test")
     args = p.parse_args()
 
-    all_tests = [
-        *sanity_check(args.name, "-sanity"),
-        *low_ratio_test(args.name),
-    ]
+    all_tests = ratio_val_test(args.name)
 
     prefix = ["python", "-m", "rsrch.hub.rl.dreamer"]
     for test in all_tests:
