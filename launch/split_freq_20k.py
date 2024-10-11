962a458aaa9e15a52524bfe6543221ea54707199
vitreusx <jakub_bednarz@protonmail.com>
2024-10-11 16:16:06 +0200

    Changes to data analysis notebooks Added a variant of split-freq test with a prefill of 20k env steps

---
+++ launch/split_freq_20k.py
@@ -0,0 +1,51 @@
+import argparse
+import shlex
+from itertools import product
+
+from .common import *
+from .sanity_check import sanity_check
+
+
+def split_freq_20k(test_name: str, suffix: str = ""):
+    common_args = ["-p", "exp.split_freq", "grid_launch"]
+    common_opts = {"_prefill": int(20e3)}
+
+    envs = ["Assault"]
+    seeds = [*range(3)]
+    wm_freqs = [1, 2, 4]
+    rl_freqs = [1, 2, 4]
+    grid = product(envs, seeds, wm_freqs, rl_freqs)
+
+    all_tests = []
+    for env, seed, wm_freq, rl_freq in grid:
+        opts = {
+            **common_opts,
+            "env.atari.env_id": env,
+            "repro.seed": seed,
+            "_wm_freq": wm_freq,
+            "_rl_freq": rl_freq,
+            "run.dir": f"runs/{test_name}/{env}-seed={seed}-wm_freq={wm_freq}-rl_freq={rl_freq}{suffix}",
+        }
+        args = [*common_args, "-o", format_opts(opts)]
+        all_tests.append(args)
+
+    return all_tests
+
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument("--test-name", default="split-freq-20k")
+    p.add_argument("--sanity", action="store_true")
+    args = p.parse_args()
+
+    all_tests = split_freq_20k(args.test_name)
+    if args.sanity:
+        all_tests.extend(sanity_check(args.test_name, "-sanity"))
+
+    prefix = ["python", "-m", "rsrch.hub.rl.dreamer"]
+    for test in all_tests:
+        print(shlex.join([*prefix, *test]))
+
+
+if __name__ == "__main__":
+    main()
---
+++ thesis/nb/utils.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import plotly.io as pio
+from ruamel.yaml import YAML
+from scipy.signal import lfilter, lfiltic
+from tbparse import SummaryReader
+
+pio.kaleido.scope.mathjax = None
+
+yaml = YAML(typ="safe", pure=True)
+
+
+class TBScalars:
+    def __init__(self, cache_dir: str | Path):
+        self.cache_dir = Path(cache_dir)
+
+    def read(self, test_dir: str | Path):
+        test_dir = Path(test_dir)
+
+        dst_path = self.cache_dir / f"{test_dir.name}.h5"
+        if not dst_path.exists():
+            board = SummaryReader(test_dir / "board")
+            dst_path.parent.mkdir(parents=True, exist_ok=True)
+            board.scalars.to_hdf(dst_path, key="scalars")
+
+        return pd.read_hdf(dst_path, key="scalars")
+
+
+def exp_mov_avg(arr: np.ndarray, alpha: float):
+    b, a = [1.0 - alpha], [1.0, -alpha]
+    zi = lfiltic(b, a, arr[:1], [0])
+    return lfilter(b, a, arr, zi=zi)[0]
