bfa71b6dd0b70476609383fa6861faebbe37c7c0
vitreusx <jakub_bednarz@protonmail.com>
2024-10-30 13:33:35 +0100

    Added an adaptive ratio test

--- rsrch/hub/rl/dreamer/runner.py
+++ rsrch/hub/rl/dreamer/runner.py
@@ -1002,3 +1002,65 @@ class Runner:
 
         if pretrain is not None:
             with_argcast(self.pretrain_rl)(**pretrain)
+
+    def adaptive_setup(
+        self,
+        init_wm_ratio: float,
+        wm_ratio_range: tuple[float, float],
+        rl_to_wm_ratio: float,
+        ratio_update_mult: float,
+    ):
+        self.min_wm_ratio, self.max_wm_ratio = wm_ratio_range
+        self.rl_to_wm_ratio = rl_to_wm_ratio
+        self.wm_ratio = init_wm_ratio
+        self.wm_ratio = min(max(self.wm_ratio, self.min_wm_ratio), self.max_wm_ratio)
+        self.exp.add_scalar("ada/wm_ratio", self.wm_ratio)
+        self.ratio_update_mult = ratio_update_mult
+
+        self.should_opt_wm = cron.Every(
+            lambda: self.env_step,
+            period=self.wm_ratio,
+            accumulate=True,
+        )
+        self.best_wm_val_loss = None
+
+        self.should_opt_rl = cron.Every(
+            lambda: self.wm_opt_step,
+            period=rl_to_wm_ratio,
+            accumulate=True,
+        )
+
+    def update_adaptive_opt(self):
+        val_loss = self.do_wm_val_epoch()
+        self.exp.add_scalar("ada/val_loss", val_loss)
+        if self.best_wm_val_loss is None:
+            self.best_wm_val_loss = val_loss
+        elif val_loss < self.best_wm_val_loss:
+            self.wm_ratio /= self.ratio_update_mult
+            self.wm_ratio = min(
+                max(self.wm_ratio, self.min_wm_ratio), self.max_wm_ratio
+            )
+            self.exp.add_scalar("ada/wm_ratio", self.wm_ratio)
+            self.should_opt_wm = cron.Every(
+                lambda: self.env_step,
+                period=self.wm_ratio,
+                accumulate=True,
+            )
+            self.best_wm_val_loss = val_loss
+        elif val_loss > self.best_wm_val_loss:
+            self.wm_ratio /= self.ratio_update_mult
+            self.wm_ratio = min(
+                max(self.wm_ratio, self.min_wm_ratio), self.max_wm_ratio
+            )
+            self.exp.add_scalar("ada/wm_ratio", self.wm_ratio)
+            self.should_opt_wm = cron.Every(
+                lambda: self.env_step,
+                period=self.wm_ratio,
+                accumulate=True,
+            )
+
+    def do_adaptive_opt_step(self):
+        while self.should_opt_wm:
+            self.do_wm_opt_step()
+            while self.should_opt_rl:
+                self.do_rl_opt_step()
