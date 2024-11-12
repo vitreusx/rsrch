dc40a6290f061dc9b08ba33437c799d84bbb79e0
vitreusx <jakub_bednarz@protonmail.com>
2024-11-12 08:11:57 +0100

    Made sac.base preset launchable

---
+++ thesis/exp/sac/base.py
@@ -0,0 +1,54 @@
+import argparse
+import shlex
+from itertools import product
+from pathlib import Path
+
+import numpy as np
+
+from ..utils import *
+
+
+def sac_base(test_name: str, suffix=""):
+    all_tests = []
+
+    common_args = [
+        "-P",
+        "thesis/exp/presets.yml",
+        "-p",
+        "thesis.sac.base",
+        "grid_launch",
+    ]
+
+    common_opts = {}
+
+    envs = A100k_MONO
+    seeds = [*range(5)]
+
+    for env, seed in product(envs, seeds):
+        opts = {
+            "env": {"type": "atari", "atari.env_id": env},
+            "repro.seed": seed,
+            "_ratio": 4,
+            "run.dir": f"runs/{test_name}/{env}-seed={seed}" + suffix,
+            **common_opts,
+        }
+        args = [*common_args, "-o", format_opts(opts)]
+        all_tests.append(args)
+
+    return all_tests
+
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument("--name", default="sac/base")
+    args = p.parse_args()
+
+    all_tests = sac_base(args.name)
+
+    prefix = ["python", "-m", "rsrch.hub.rl.dreamer"]
+    for test in all_tests:
+        print(shlex.join([*prefix, *test]))
+
+
+if __name__ == "__main__":
+    main()
