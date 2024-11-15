57df82dd946e883dff9fcee18e02f080b6a3a5f7
vitreusx <jakub_bednarz@protonmail.com>
2024-11-15 07:31:12 +0100

    Added shift tasks to the data augmentation sweep test

--- thesis/exp/data_aug/sweep.py
+++ thesis/exp/data_aug/sweep.py
@@ -22,6 +22,8 @@ def data_aug_sweep(test_name, suffix=""):
     seeds = [*range(5)]
     ratio = 8
     drq_configs = {
+        "shift4": dict(type="shift", max_shift=4),
+        "shift2": dict(type="shift", max_shift=2),
         "cutout": dict(type="cutout", apply_prob=0.5),
         "vflip": dict(type="vflip", apply_prob=0.1),
         "rotate": dict(type="rotate", rotate_deg=5.0),
