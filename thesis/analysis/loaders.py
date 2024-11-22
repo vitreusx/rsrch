dea49774e9c901d9fce5c14c0cc4e334616c1e46
vitreusx <jakub_bednarz@protonmail.com>
2024-11-22 13:16:54 +0100

    Added the analyses of the preparatory experiment for the benchmark, and the plasticity loss.

--- thesis/analysis/loaders.py
+++ thesis/analysis/loaders.py
@@ -331,3 +331,57 @@ def sac_ent_sched2_loader():
     res_df = pd.DataFrame.from_records(res_df)
 
     return res_df, scalars
+
+
+def warm_start_loader():
+    results_dir = Path("../results/warm_start")
+    scalars = TBScalars(".cache/warm_start")
+
+    res_df = []
+    for test in tqdm([*results_dir.iterdir()]):
+        params = test.name.split("-")
+        test_r = {}
+        test_r["env"] = params[0]
+        types = {"seed": int}
+        for (name, typ), value in zip(types.items(), params[1:]):
+            test_r[name] = typ(value.removeprefix(f"{name}="))
+        df = scalars.read(test)
+        final_val = df[df["tag"] == "val/mean_ep_ret"].iloc[-1]
+        if final_val["step"] >= 6e6:
+            test_r["score"] = final_val["value"]
+            res_df.append({"path": test, **test_r})
+    res_df = pd.DataFrame.from_records(res_df)
+
+    return res_df, scalars
+
+
+def final_prelim_loader():
+    results_dir = Path("../results/final/prelim")
+    scalars = TBScalars(".cache/final/prelim")
+
+    res_df = []
+    for test in tqdm([*results_dir.iterdir()]):
+        params = test.name.split("-")
+        test_r = {}
+        test_r["env"] = params[0]
+        types = {"cfg": str, "seed": int}
+        for (name, typ), value in zip(types.items(), params[1:]):
+            test_r[name] = typ(value.removeprefix(f"{name}="))
+
+        cfg = test_r["cfg"]
+        rl_ratio, num_epochs = (int(x) for x in cfg.split("x"))
+        del test_r["cfg"]
+        test_r = {
+            **test_r,
+            "rl_ratio": rl_ratio,
+            "num_epochs": num_epochs,
+        }
+
+        df = scalars.read(test)
+        final_val = df[df["tag"] == "val/mean_ep_ret"].iloc[-1]
+        if final_val["step"] >= 400e3:
+            test_r["score"] = final_val["value"]
+            res_df.append({"path": test, **test_r})
+    res_df = pd.DataFrame.from_records(res_df)
+
+    return res_df, scalars
