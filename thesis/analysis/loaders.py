202c5ddd8365151e465d0689335f98c7c30ccb1c
vitreusx <jakub_bednarz@protonmail.com>
2024-11-28 07:49:36 +0100

    Added MPC scores and more context for the analysis of Atari-100k benchmark

--- thesis/analysis/loaders.py
+++ thesis/analysis/loaders.py
@@ -488,3 +488,25 @@ def final_dreamerv2_loader():
     res_df = pd.DataFrame.from_records(res_df)
 
     return res_df, scalars
+
+
+def mpc_base_loader():
+    results_dir = Path("../results/mpc/base")
+    scalars = TBScalars(".cache/mpc/base")
+
+    res_df = []
+    for test in tqdm([*results_dir.iterdir()]):
+        params = test.name.split("-")
+        test_r = {}
+        test_r["env"] = params[0]
+        types = {"ratio": int, "seed": int}
+        for (name, typ), value in zip(types.items(), params[1:]):
+            test_r[name] = typ(value.removeprefix(f"{name}="))
+        df = scalars.read(test)
+        final_val = df[df["tag"] == "val/mean_ep_ret"].iloc[-1]
+        if final_val["step"] >= 400e3:
+            test_r["score"] = final_val["value"]
+            res_df.append({"path": test, **test_r})
+    res_df = pd.DataFrame.from_records(res_df)
+
+    return res_df, scalars
