80c3d18a9b615c6f689c8ff4aa9f5adbaaa1f0ec
vitreusx <jakub_bednarz@protonmail.com>
2025-08-14 20:26:40 +0200

    Cleaned up CIFAR loaders

--- rsrch/data/cifar.py
+++ rsrch/data/cifar.py
@@ -1,1 +1,64 @@
-from torchvision.datasets import CIFAR10, CIFAR100  # noqa: F401
+from pathlib import Path
+from typing import Literal
+
+from ruamel.yaml import YAML
+from torchvision import datasets
+
+from .meta import ClsMeta
+
+
+class CIFAR10:
+    """[CIFAR10](https://www.cs.toronto.edu/~kriz/cifar.html) dataset."""
+
+    def __init__(
+        self,
+        root: str | Path,
+        split: Literal["train", "test"],
+    ):
+        self.base = datasets.CIFAR10(root, train=(split == "train"))
+
+    def __len__(self):
+        return len(self.base)
+
+    def __getitem__(self, index):
+        image, label = self.base[index]
+        return {"image": image, "label": label}
+
+    @staticmethod
+    def meta():
+        yaml = YAML(typ="safe", pure=True)
+        with open(Path(__file__).parent / "cifar10.yml", "r") as f:
+            data = yaml.load(f)
+        return ClsMeta(data)
+
+
+class CIFAR100:
+    """[CIFAR100](https://www.cs.toronto.edu/~kriz/cifar.html) dataset."""
+
+    def __init__(
+        self,
+        root: str | Path,
+        split: Literal["train", "test"],
+    ):
+        self.base = datasets.CIFAR100(root, train=(split == "train"))
+
+    def __len__(self):
+        return len(self.base)
+
+    def __getitem__(self, index):
+        image, label = self.base[index]
+        return {"image": image, "label": label}
+
+    @staticmethod
+    def meta():
+        yaml = YAML(typ="safe", pure=True)
+        with open(Path(__file__).parent / "cifar10.yml", "r") as f:
+            data = yaml.load(f)
+
+        return ClsMeta(
+            {
+                "classes": {
+                    label: item["class"] for label, item in data["classes"].items()
+                }
+            }
+        )
